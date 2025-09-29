af57e2b950375ec71bfe85dea76e8544ceee32f5
Diastriker <qortmfahr@gmail.com>
2025-09-29 15:53:11 +0900

    @Controller 만 추가

--- src/main/java/com/storemanager/in/InController.java
+++ src/main/java/com/storemanager/in/InController.java
@@ -1,5 +1,10 @@
 package com.storemanager.in;
 
+import org.springframework.stereotype.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class InController {
 
 }
