ed29fcfe514155151f2ef13c708181dd2e7b05fe
KimJohnhoi <johnhoi99@gmail.com>
2025-09-30 17:28:10 +0900

    0930 out ok

--- src/main/java/com/storemanager/out/OutController.java
+++ src/main/java/com/storemanager/out/OutController.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,6 +41,4 @@ public class OutController {
 		json = new JSONObject(target);
 		return json.toString();
 	}
-	
-	
 }
