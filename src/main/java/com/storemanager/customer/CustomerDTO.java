d570782f51e32696de88e0950af3f923d95b7f5d
excitedhee@gmail.com <excitedhee@gmail.com>
2025-10-01 17:52:56 +0900

    version 0.0.6

--- src/main/java/com/storemanager/customer/CustomerDTO.java
+++ src/main/java/com/storemanager/customer/CustomerDTO.java
@@ -1,21 +1,25 @@
 package com.storemanager.customer;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CustomerDTO {
-	private int    gcm_idx;
-	private String gcm_code;
-	private String gcm_name;
-	private int	   gcm_email;
-	private String gcm_addr;
-	private String gcm_tel;
-	private String gcm_regdate;
-	private String gcm_del_flag;
+	private Long    gcm_Idx;  //pk
+	private String   gcm_Code;  // 거래처 코드
+	private String gcm_Name;  // 거래처명
+	private String gcm_Email; // 이메일
+	private String gcm_Addr;  // 주소
+	private String gcm_Tel;   // 전화/ 휴대폰
+	private LocalDateTime gcm_Regdate; //등록일
+	private String gcm_Del_Flag;  // 'N' /'Y'
 	
 	
 }
