8405b66e9db9cbcf08345bb2ee0bc7ad0f28215d
hulqee127-tech <hulqee127@gmail.com>
2025-10-02 09:04:24 +0900

    25-10-02-complete

--- src/main/java/com/storemanager/bill/BillController.java
+++ src/main/java/com/storemanager/bill/BillController.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,5 +28,30 @@ public class BillController {
 		mv.addObject("billList",billList);
 		mv.setViewName("bill/bill");
 		return mv;
+	}
+	
+	@GetMapping("bill/onebill/{id}")
+	@ResponseBody
+	public List<BillDTO> oneBill(ModelAndView mv,BillDTO billDto, @PathVariable("id") Long id) {
+		//System.out.println("id : "+id);
+
+		List<BillDTO> oneBill = billMapper.getOneBill(id);
+		//return mv;
+		return oneBill;
+	}
+	
+	@GetMapping("/bill/billout")
+	@ResponseBody
+	public List<BillDTO> billOutList(ModelAndView mv, BillDTO billDto) {
+		
+		LocalDate endDay = LocalDate.now();
+		LocalDate startDay = endDay.minusDays(7);
+
+		// List
+		List<BillDTO> billOutList = billMapper.getBillListOUT(startDay.toString(), endDay.toString());
+		//System.out.println(billOutList);
+		//mv.addObject("billList",billList);
+		//mv.setViewName("bill/bill");
+		return billOutList;
 	}
 }
