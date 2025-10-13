3b06cb778b5223bd83cc35af55b6352ab6bce1b7
Seo-JS0823 <seong0093571@naver.com>
2025-10-13 09:45:24 +0900

    10-13 work start

--- src/main/resources/static/js/juso.js
+++ src/main/resources/static/js/juso.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 const config = {
 	apikey:'devU01TX0FVVEgyMDI1MDkzMDIyMzMyMjExNjI5NTI='
-=======
-const config = {
-	apikey:'devU01TX0FVVEgyMDI1MTAwMTExMDUxNzExNjI5Njg='
->>>>>>> item
 }
